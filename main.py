72ad7e7b5c5cf97f6a1302b3dcd8b32e2a450d98
Camilo Sánchez <camilosanchez@MacBook-Pro-de-Camilo.local>
2022-08-26 13:00:43 -0500

    Se obtienen regiones, países y lenguajes

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-def main():
-    pass
+import requests
+
+def getinfo():
+    url = "https://restcountries.com/v3.1/all"
+    data = requests.get(url)
+    data = data.json()
+
+    for element in data:
+        print(element['region'], "-", element['translations']['spa']['common'], "-")
+        if ("languages" in element):
+            for value in element['languages'].values():
+                print(value)
+
+
+if __name__ == '__main__':
+    getinfo()
